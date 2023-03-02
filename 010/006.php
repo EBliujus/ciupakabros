f7d7fb6f69fa39901c86d6d9800e971daad58c30
EBliujus <ev.bliujus@gmail.com>
2023-03-02 19:56:09 +0200

    006

--- 010/006.php
+++ 010/006.php
@@ -2,8 +2,8 @@
 /* 6. Sukurti kintamąjį su stringu: “An American in Paris”. Suskaičiuoti visas “a” (didžiąsias ir mažąsias) raides. Rezultatą atspausdinti. */
 
 $text = 'An American in Paris';
-// $newText = strtoupper($text);
-print_r(count_chars($text,1));
+$newText = strtoupper($text);
+$textArr= count_chars($newText,1);
 
 /* $textArr = count_chars($text,1);
 
