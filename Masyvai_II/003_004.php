9893d21475de76905de0868445365a36bc96668f
EBliujus <ev.bliujus@gmail.com>
2023-03-07 15:36:09 +0200

    6 uzdavinys

--- Masyvai_II/003_004.php
+++ Masyvai_II/003_004.php
@@ -13,3 +13,7 @@ for ($i = 0; $i < 10; $i++) {
 }
 echo '<pre/>';
 print_r($arr);
+echo '---------------------------', '<br/>';
+
+
+ /* 4. Išrūšiuokite trečio uždavinio pirmo lygio masyvą taip, kad elementai kurių masyvai trumpiausi eitų pradžioje. Masyvai kurie turi bent vieną “K” raidę, visada būtų didžiojo masyvo visai pradžioje. */ 
--- Masyvai_II/005_006.php
+++ Masyvai_II/005_006.php
@@ -12,3 +12,22 @@ foreach (range(0, 30) as $i) {
 }
 echo '<pre>';
 print_r($arr);
+echo '-------------------------', '<br/>';
+
+/* 6. Išrūšiuokite 5 uždavinio masyvą pagal user_id didėjančia tvarka. Ir paskui išrūšiuokite pagal place_in_row mažėjančia tvarka.
+*/
+
+  $sortId = fn($a, $b) => $a['user_id'] - $b['user_id'];
+  usort($arr, $sortId);
+
+  print_r($arr);
+echo '----------------------------', '<br/>';
+
+  $sortRow = fn($a, $b) => $b['place_in_row'] - $a['place_in_row'];
+  usort($arr,$sortRow);
+
+  print_r($arr);
+
+
+
+   
