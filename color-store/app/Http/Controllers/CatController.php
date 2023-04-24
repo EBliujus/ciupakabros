cdd2cfa7dbd2d835e10a5ff148fb92bac45678e5
EBliujus <ev.bliujus@gmail.com>
2023-04-24 13:59:39 +0300

    new project

---
+++ color-store/app/Http/Controllers/CatController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Cat;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreCatRequest;
+use App\Http\Requests\UpdateCatRequest;
+
+class CatController extends Controller
+{
+
+    public function index()
+    {
+        //
+    }
+
+
+    public function create()
+    {
+        //
+    }
+
+
+    public function store(StoreCatRequest $request)
+    {
+        //
+    }
+
+
+    public function show(Cat $cat)
+    {
+        //
+    }
+
+    public function edit(Cat $cat)
+    {
+        //
+    }
+
+
+    public function update(UpdateCatRequest $request, Cat $cat)
+    {
+        //
+    }
+
+
+    public function destroy(Cat $cat)
+    {
+        //
+    }
+}
